ffe8d19d0445f0be36440c41e3ef737e74fd8b17
nkho29 <nazar17ho@gmail.com>
2023-10-05 21:04:09 +0300

    lesson_8

---
+++ lesson_7/data_types_6.py
@@ -0,0 +1,21 @@
+# незнмінні
+str1 = "Helloooooooo"
+set1 = []
+bool1 = True, False
+int1 = 20
+flo1 = 20.2
+
+# зміінні
+dict1 = {
+    "key": "value",
+    "price": 20
+}
+
+print(dict1.keys())
+print(dict1.values())
+
+
+list1 = ("hello", 20, 3.3)
+
+set1 = {20, 30, 40, 50, 60, 70, 2}
+print(set1)
---
+++ lesson_8/lesson_8.py
@@ -0,0 +1,26 @@
+num_ = 44
+str_num = 55
+
+dict_empty = {}
+set_empty = {}
+
+print(f"Task 1: {type(dict_empty)}, {type(set_empty)}")
+
+sum_num = num_ + int(str_num)
+# sum_num = str(num_) + str_num
+
+print(sum_num)
+
+print(int(bool(num_)))
+print(int(True))
+print(int(False))
+
+list_num = [265, 10, 89, 2, 2, 2]
+print(set(list_num))
+
+print("This is True or False {1}, {0}".format(bool(int(str_num)),"Not + False", tree=not True))
+
+print("This is %d apple" % 25)
+
+
+
